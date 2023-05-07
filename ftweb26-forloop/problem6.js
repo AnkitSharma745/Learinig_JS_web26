b8de0647dea3c5f8fa1a9a4e5621232a7187acf6
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-05-07 23:53:27 +0530

    for loop has been done

---
+++ ftweb26-forloop/problem6.js
@@ -0,0 +1,11 @@
+let x= "";// empty string
+
+// string + number= string //concatination.
+
+
+// console.log(typeof(2)); // number
+
+// console.log(typeof(2+x)); // string.
+
+
+console.log(2+ " "+ 3);
