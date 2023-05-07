7137819acfd67258ae445b618de6bd6fab7d7b20
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-05-07 23:52:24 +0530

    for loop has been done

---
+++ ftweb26-forloop/problem3.js
@@ -0,0 +1,21 @@
+//print masai for 5 times.
+
+// 1. starting point.
+
+//2. condition.
+
+//3. incre/ dec 
+
+//4. output.
+
+
+for(let i=1; i<=5; i++ ){
+
+  // console.log("masai");
+}
+
+1
+2
+3
+4
+5
