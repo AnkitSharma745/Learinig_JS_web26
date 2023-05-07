b682000b2564830391ab91f8694ecf4d85f9ecff
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-05-07 23:52:40 +0530

    for loop has been done

---
+++ ftweb26-forloop/problem4.js
@@ -0,0 +1,19 @@
+// print the sum of numbers 1-10 using for loop.
+
+
+// 1. starting point.
+// 2. condition
+// 3. ince/dec.
+// 4. output---> sum of number.
+
+//   // where should I declare a variable sum .
+
+//   after the loops end
+// // outside the loop console.log(sum)
+
+let sum=0;
+for(let i=1; i<=10; i++){
+  sum= sum+i;
+}
+
+console.log(sum);
