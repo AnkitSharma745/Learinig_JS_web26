0cafad3bc6ce6993c20d88073782f7241cffd5a3
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-05-07 23:52:56 +0530

    for loop has been done

---
+++ ftweb26-forloop/problem5.js
@@ -0,0 +1,20 @@
+//print the number which are divisible by 2 and 5;
+
+1-10;
+
+// I want the numbers which are divisible by 2 and 5;
+
+
+for(let i=1; i<=10;i++){
+  
+  if((i%2==0) && (i%5==0)){
+    console.log(i);
+  }
+
+// if(i%2==0){
+//   if(i%5==0){
+//     console.log(i);
+//   }
+// }
+  
+}
