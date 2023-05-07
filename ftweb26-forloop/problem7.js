238ad550076aeb307e2b465d1adf7ad86c83ba9f
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-05-07 23:53:42 +0530

    for loop has been done

---
+++ ftweb26-forloop/problem7.js
@@ -0,0 +1,14 @@
+// print numbers in horizontal number.
+
+
+123456 //  horizontal manner.
+
+1-5 // horzontal 
+
+let x= ""; // empty string
+
+for(let i=1; i<=5;i++){
+  x= x + i + " ";
+}
+
+console.log(x);
