8d2f0ff9519f793fdf8f9a67cec373cc15b4f6bf
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-05-08 23:09:15 +0530

    Todays has been done

---
+++ ftweb26-forloop/problem11.js
@@ -0,0 +1,14 @@
+//print numbers 1-5;
+
+
+
+for(let i=1; i<=5; i++){
+  console.log(i);
+  
+  if(i==4){
+    continue;
+  }
+  
+}
+
+//continue===> skipping the below code and moving to the next part.
