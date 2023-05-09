2aaf774570a540f9c24b1e232f6b760074f21f33
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-05-09 22:53:59 +0530

    Object has been done

---
+++ objects/problem6.js
@@ -0,0 +1,16 @@
+let details= {
+name: "goku",
+age : 20 ,
+address: "hyderabad",
+phone: 123456,
+friend: "vegita"
+}
+
+
+//update the already present data.
+
+details.friend= "freeza"; // dot notation
+
+details["address"]= "delhi"; //bracket notation.
+
+console.log(details);
