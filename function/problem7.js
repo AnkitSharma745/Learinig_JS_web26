e74c5c6f13e3803cab91be76d883d4ad39aa024b
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-05-09 23:15:06 +0530

    FUnction has been done

---
+++ function/problem7.js
@@ -0,0 +1,30 @@
+// function sum(){
+//   let a= 1;
+//   let b= 2;
+
+//   console.log(a+b);
+// }
+
+// sum();
+// sum();
+// sum(); // I will get the same output.
+
+
+function sum(a,b){
+  console.log(a+b);
+}
+
+sum(1,2);
+sum(100,3);
+
+
+function sub(c,d){
+  console.log(c-d);
+}
+
+sub(10,2);
+
+function mul(e,f){
+  console.log(e*f);
+}
+mul(10,2);
