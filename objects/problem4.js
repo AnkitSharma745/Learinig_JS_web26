49f7f6a9c57ae92948b355b4aa030f68b0d521a9
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-05-09 22:53:30 +0530

    Object has been done

---
+++ objects/problem4.js
@@ -0,0 +1,22 @@
+let details= {
+name: "goku",
+age : 20 ,
+address: "hyderabad",
+phone: 123456,
+friend: "vegita"
+}
+
+// console.log(details) // this will give us the entire object 
+
+
+// if I want to print specific values in the object.
+
+//1 way 
+// dot notation
+console.log(details.name);
+console.log(details.age);
+
+//2 way
+// bracket notation
+console.log(details["address"]);
+console.log(details["phone"]);
