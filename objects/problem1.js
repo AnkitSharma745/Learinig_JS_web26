eb3f69ff06eec5f7c221b7713e228f1e2574ad78
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-05-09 22:52:51 +0530

    Object has been done

---
+++ objects/problem1.js
@@ -0,0 +1,10 @@
+for(let floor=1; floor<=4; floor++){
+  // console.log("floor", floor);
+  for(let room=1; room<=3; room++){
+    if(room==2){
+      break;
+    }
+  console.log(floor+ "-"+ room);
+}
+}
+
