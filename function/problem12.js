6890d6c974aa9547d9fe88661a9100e0d65f7231
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-05-09 23:16:26 +0530

    FUnction has been done

---
+++ function/problem12.js
@@ -0,0 +1,18 @@
+function add(a,b){
+  return a+b;
+  // console.log(a+b);
+}
+
+let result= add(1,2);
+
+// console.log(result);
+
+function multiple(c){
+
+  return c*2;
+}
+
+
+let result2= multiple(result);
+
+console.log(result2);
