bf1c3341d9972b3917ab0e011365c83a53b41f50
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-05-09 22:54:39 +0530

    Object has been done

---
+++ objects/problem9.js
@@ -0,0 +1,16 @@
+let details= {
+name: "goku",
+age : 20 ,
+address: "hyderabad",
+phone: 123456,
+friend: "vegita",
+}
+
+
+// delete the key and value in Object
+
+delete details.friend;
+
+delete details["address"];
+
+console.log(details);
