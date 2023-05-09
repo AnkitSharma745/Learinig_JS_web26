22f0502b683c3c7b041406b3ddfc593b208b2ab1
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-05-09 22:53:45 +0530

    Object has been done

---
+++ objects/problem5.js
@@ -0,0 +1,16 @@
+let details= {
+name: "goku",
+age : 20 ,
+address: "hyderabad",
+phone: 123456,
+friend: "vegita"
+}
+
+
+// add into the object 
+
+details.hobbie= "playing pc games" // dot notation.
+
+details["father"] = "luffy"; // bracket notation.
+
+console.log(details);
