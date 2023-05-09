46b5650014f1784644bec19c2cd9e20ff80913c6
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-05-09 23:08:48 +0530

    FUnction has been done

---
+++ function/problem2.js
@@ -0,0 +1,24 @@
+// syntax
+
+// function name(){
+
+ // write the code here
+
+// }
+
+
+function Instagram(){
+
+  console.log("please open instagram");
+}
+
+Instagram() // I am calling the function.
+
+
+// function declaration.
+function whatsapp(){
+  console.log("please open whatsapp");
+}
+
+
+whatsapp() // calling a function.
