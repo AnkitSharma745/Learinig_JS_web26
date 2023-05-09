70cae9eabdc37b2b1180d87fba1172fca5e13f8c
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-05-09 22:54:13 +0530

    Object has been done

---
+++ objects/problem7.js
@@ -0,0 +1,15 @@
+let details= {
+name: "goku",
+age : 20 ,
+address: "hyderabad",
+phone: 123456,
+friend: "vegita",
+hobbies: ["cricket","pc games", "football"]
+ 
+}
+
+// console.log(details);
+
+console.log(details.hobbies[0]); // dot notation.
+
+console.log(details["hobbies"][1]); // bracket notation.
