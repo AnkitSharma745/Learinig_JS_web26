8711b8810f6f404b19eeeae45872baec223b3e70
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-05-09 23:10:12 +0530

    FUnction has been done

---
+++ function/problem3.js
@@ -0,0 +1,32 @@
+// function declaration
+function sum(){
+  let a= 1;
+  let b= 2;
+
+  console.log(a+b);
+}
+
+sum(); // calling a function.
+
+
+// subtraction
+// multiplication
+
+subraction()// calling a function.
+
+function subraction(){
+  let x= 1;
+  let y=2;
+
+  console.log(y-x);
+}
+
+
+function multiplication(){
+  let r=1;
+  let y=2;
+
+  console.log(r*y);
+}
+
+multiplication();
