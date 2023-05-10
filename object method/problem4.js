a9c12298017dd9a6b982d4eb0bfffc41709a2097
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-05-10 23:44:37 +0530

    Object method has been done

---
+++ object method/problem4.js
@@ -0,0 +1,15 @@
+let form = {
+  detail: (name , age)=>{
+    console.log(name);
+    console.log(age);
+  }
+}
+
+form.detail( );
+
+
+
+
+
+// the default parameter values are what ?
+// undefined.
