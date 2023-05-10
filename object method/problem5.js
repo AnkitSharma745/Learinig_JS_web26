894c8501f263b2e3d5598b1e25834bba1d08ce8e
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-05-10 23:44:57 +0530

    Object method has been done

---
+++ object method/problem5.js
@@ -0,0 +1,12 @@
+// create a object and inside that  object take a function, which should add two number and return them.
+
+let form= {
+  
+  sum : function (a,b){
+    return a+b;
+  }
+}
+
+let output= form.sum(1,2);
+
+console.log(output);
