a7ccfe3e7d712a1eca79a4231bf82cdbf9748d89
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-05-10 23:48:39 +0530

    Higher order functio has been done

---
+++ Higher oder function/problem3.js
@@ -0,0 +1,11 @@
+ // variable can have values as number , string, boolean, array, object, function, [], "", {}, null, variable.
+
+// let a= 10;
+
+// let b= a;
+
+
+let a= function (){
+  console.log("hello");
+}
+// anonymous function or function expression
