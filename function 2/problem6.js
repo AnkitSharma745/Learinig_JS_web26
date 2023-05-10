7294bac4d14e36581ff0184300ffe085e09d1877
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-05-10 23:41:53 +0530

    Function2 has been done

---
+++ function 2/problem6.js
@@ -0,0 +1,24 @@
+//1 normal function 
+function name(x){
+  console.log(x);
+}
+name("luffy");
+
+
+//2. anonymous function.
+
+let print = function (b){
+  
+  console.log(b);
+}
+
+print("zoro");
+
+
+//3. arrow function or anonymous function.
+
+let print2=  (c) =>{
+  console.log(c);
+}
+
+print2("nami");
