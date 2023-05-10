be065980c59ac007216a51d391f8ea2bea6aef6c
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-05-10 23:42:31 +0530

    Function2 has been done

---
+++ function 2/problem8.js
@@ -0,0 +1,10 @@
+print();
+
+typeof();
+
+console.log();
+
+Number();--> string to a number.
+
+  push()
+pop();
---
+++ function 2/problem9.js
@@ -0,0 +1,23 @@
+//guide / instruction.
+
+
+//array--> search
+
+//pop();
+//push();
+//shift();
+//unshift();
+// splice();
+// join();
+// indexof();
+// include();
+
+
+//strings
+
+// touppercase()
+//tolowercase()
+//concat();
+//indexof();
+//include();
+//bold()
