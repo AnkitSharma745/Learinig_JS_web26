c88a192cbd7116e9a1d23bab3b60bbc96855ab03
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-05-10 23:43:52 +0530

    Object method has been done

---
+++ object method/problem1.js
@@ -0,0 +1,26 @@
+// 1. normal function
+// 2. anonymous function
+// 3. arrow function or anonymous function.
+
+//1
+function print(){
+  console.log("hello");
+}
+
+print();
+
+//2. anonymous function
+
+let print1= function (){
+  console.log("everyone");
+}
+
+print1();
+
+//3. arrow function
+
+let print3= ()=>{
+  console.log("I hope you are doing good");
+}
+
+print3();
