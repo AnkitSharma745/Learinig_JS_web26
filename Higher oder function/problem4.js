3d1a2c262a6671d88bbf051e35dfd62837b80563
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-05-10 23:48:51 +0530

    Higher order functio has been done

---
+++ Higher oder function/problem4.js
@@ -0,0 +1,11 @@
+// parameter and argument.
+
+
+// here a, b are called parameter.
+the parameter can also have values and variable.
+function sum(a,b){
+  console.log(1+b);
+}
+
+sum(1,2);
+// here the values are called argument-> variable, number, array, string, object, function
