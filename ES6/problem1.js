4a5040a4d8569957ab3c1409545ecacca79ab802
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-05-11 22:20:38 +0530

    ES6 has been done

---
+++ ES6/problem1.js
@@ -0,0 +1,113 @@
+// ES6-- ecma script 6
+
+//3 ways of declaring a variable.
+
+//ES5
+// var a=10;
+// console.log(a);
+
+//ES6
+// let and const
+
+
+// var a=10;
+// let b= 20;
+// const c= 30;
+
+// console.log(a,b,c);
+
+
+
+// {
+//   // block of code
+//   // let and const 
+// } // block
+
+// var a=10;
+
+// {
+//   var a= 20;
+// }
+// console.log(a);
+
+
+//  var b= 30;
+// {
+//   b= 10;
+//   console.log(b);
+// }
+// var is used to declare the variable outside the block scope 
+
+
+// let and const
+
+// let values can be changed 
+// but const values cannot be changed.
+
+// let a= 10;
+// a= 30;
+// console.log(a);
+
+// const b= 45;
+// b= 1;
+// console.log(b);
+
+
+// let and var work in a same way when you try to declare outside the block scope
+
+// but the let declaration changes when used inside the block scope.
+
+
+// var ---> let, const.
+// Es5 ---> Es6
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
