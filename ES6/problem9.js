8856176aae135952e1721f97c989559680fd15ee
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-05-11 22:22:22 +0530

    ES6 has been done

---
+++ ES6/problem9.js
@@ -0,0 +1,25 @@
+// console.log("hello");
+// console.log("world");
+
+// console.log("hello\nworld");
+// console.log("hello" + "\n"+ world);
+
+
+// template strings or template literals.
+
+let name= `goku`;
+let age= 12;
+
+// my name is goky and
+// I am 12 years old
+
+console.log(`my name is ${name} and
+I am ${age} years old`);
+
+
+console.log(`hello
+world
+hi
+everyone
+how are you
+I am doing good`)
