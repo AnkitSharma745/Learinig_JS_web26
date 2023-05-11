50dd09846dd13c06663c05589d39a919a63026df
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-05-11 22:19:33 +0530

    Higher order funciton has been done

---
+++ Higher oder function/problem11.js
@@ -0,0 +1,20 @@
+// 4. foreach
+
+
+let arr= [2,-5, 10,15,20];
+
+arr.forEach(function (c){
+  console.log(c);
+  // return c; we cannot return because inside the foreach inbuilt function there is no  return statement.
+})
+
+
+
+
+
+// function print(){
+//   console.log("hello");
+// }
+
+// let a=print();
+// console.log(a);
