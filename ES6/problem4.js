3c49fe994b75415ac3626b17785f2a2e79687cb6
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-05-11 22:21:12 +0530

    ES6 has been done

---
+++ ES6/problem4.js
@@ -0,0 +1,34 @@
+// rest operator---> it convert all the argument into array.
+// (...y)
+
+
+function print(...a){
+  console.log(a);
+}
+
+print(1,2,3,4,5,6);
+
+
+
+// spread operator---> it used to copy elements from one array into another array and it is used to copy elements from one object into another object.
+// let arr= [1,2,3];
+
+// let arr2= ["goku", "luffy"];
+
+// let arr3= [...arr, ...arr2];
+
+// console.log(arr3);
+
+
+// let obj= {
+//   name: "luffy",
+//   age:12
+// }
+
+// let obj2= {
+//   ...obj,
+//   address: "hyderbad"
+// }
+
+// console.log(obj2)
+
